2ee1485f54e0e07d3426a503881b5f6b289fe525
Jordan <jordansampson23@gmail.com>
2023-01-22 14:26:44 -0600

    testing getters

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import newTask from './javascript/task';
 
 const item = newTask('Mow the yard');
 
-console.log(item)
+console.log(item.name)
+item.taskName = "Cut the Grass"
+console.log(item.name)
