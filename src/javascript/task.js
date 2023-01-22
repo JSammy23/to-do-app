3836d7d1883b683a003513d758bae34d49e29ac5
Jordan <jordansampson23@gmail.com>
2023-01-22 13:58:06 -0600

    task module intiliazed

---
+++ src/javascript/task.js
@@ -0,0 +1,13 @@
+const newTask = (name) => {
+    let note = ""
+    let dueDate = ''
+    let completed = false
+    let priority = 'normal';
+
+    
+    
+    return {name, note, dueDate, completed, priority}
+
+}
+
+export default newTask
