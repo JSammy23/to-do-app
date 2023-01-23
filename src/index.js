1100bd9c69025c3555d414cd61dcb6ef1448d2e7
Jordan <jordansampson23@gmail.com>
2023-01-23 09:33:12 -0600

    import date-fns

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import './styles.css';
 import newTask from './javascript/task';
+import { compareAsc, format } from 'date-fns'
 
 
 const item = newTask('Mow the yard');
 
-console.log(item.name)
-item.taskName = "Cut the Grass"
-console.log(item.name)
+
+
+item.note = 'Rain coming Thursday'
+
+item.dueDate = format(new Date(2023, 0, 24), 'MM/dd/yyyy')
+
+console.log(item.dueDate)
