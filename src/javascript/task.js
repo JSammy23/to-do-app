0df3dd8ef5b349c4f8391c6ff7dfc2c41f47a56c
Jordan <jordansampson23@gmail.com>
2023-01-23 09:15:59 -0600

    newTask factory working, no getters

--- src/javascript/task.js
+++ src/javascript/task.js
@@ -5,50 +5,52 @@ const newTask = (name) => {
     let completed = false
     let priority = 'normal';
 
-    
-    
     return {
-        get name() {
-            return taskName;
-        },
-
-        get note() {
-            return note;
-        },
-
-        get dueDate() {
-            return dueDate;
-        },
-
-        get completed() {
-            return completed;
-        },
-        
-        get priority() {
-            return priority;
-        },
-
-        set taskName(name) {
-            return taskName = name;
-        },
-
-        set writeNote(note) {
-            return note = note;
-        },
-
-        set date(date) {
-            return dueDate = date
-        },
-
-        set done(status) {
-            return completed = status
-        },
-
-        set priority(status) {
-            return priority = status;
-        }
-
+        taskName, note, dueDate, completed, priority
     }
+    
+    // return {
+    //     get name() {
+    //         return taskName;
+    //     },
+
+    //     get note() {
+    //         return note;
+    //     },
+
+    //     get dueDate() {
+    //         return dueDate;
+    //     },
+
+    //     get completed() {
+    //         return completed;
+    //     },
+        
+    //     get priority() {
+    //         return priority;
+    //     },
+
+    //     set taskName(name) {
+    //         return taskName = name;
+    //     },
+
+    //     set writeNote(desc) {
+    //         note = desc;
+    //     },
+
+    //     set date(date) {
+    //         return dueDate = date
+    //     },
+
+    //     set done(status) {
+    //         return completed = status
+    //     },
+
+    //     set priority(status) {
+    //         return priority = status;
+    //     }
+
+    // }
 
 }
 
