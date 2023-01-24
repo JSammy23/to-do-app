f920a8a3fda531b94014cca9be9536ec21557a67
Jordan <jordansampson23@gmail.com>
2023-01-24 13:27:00 -0600

    module imports

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import './styles.css';
-import newTask from './javascript/task';
+import Task, {handleForm} from './javascript/task';
 import { compareAsc, format } from 'date-fns'
 import newProject, { projectList } from './javascript/project';
 
 
 
-const item = newProject('item')
 
-item.addTask(newTask('Mow the yard'))
 
-console.log(item)
 
-console.log(projectList)
 
-projectList.add(item)
