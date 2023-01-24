084e51c5413500dfe3d80efa1b70d3f87dd4015f
Jordan <jordansampson23@gmail.com>
2023-01-24 11:23:19 -0600

    Task module takes taskDetails object

--- src/javascript/task.js
+++ src/javascript/task.js
@@ -1,9 +1,23 @@
-const Task = (name) => {
-    let taskName = name
-    let note = ""
-    let dueDate = ''
-    let completed = false
-    let priority = 'normal';
+// const Task = (name) => {
+//     let taskName = name
+//     let note = ""
+//     let dueDate = ''
+//     let completed = false
+//     let priority = 'normal';
+
+//     return {
+//         taskName, note, dueDate, completed, priority
+//     }
+    
+    
+// }
+
+const Task = (taskDetails) => {
+    let taskName = taskDetails.name
+    let note = taskDetails.note
+    let dueDate = taskDetails.dueDate
+    let completed = taskDetails.completed
+    let priority = taskDetails.priority;
 
     return {
         taskName, note, dueDate, completed, priority
