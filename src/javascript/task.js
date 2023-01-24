700befb8372af354e99df36e42c36c45d608ea6a
Jordan <jordansampson23@gmail.com>
2023-01-24 10:53:07 -0600

    Renamed Task module

--- src/javascript/task.js
+++ src/javascript/task.js
@@ -1,4 +1,4 @@
-const newTask = (name) => {
+const Task = (name) => {
     let taskName = name
     let note = ""
     let dueDate = ''
@@ -9,49 +9,7 @@ const newTask = (name) => {
         taskName, note, dueDate, completed, priority
     }
     
-    // return {
-    //     get name() {
-    //         return taskName;
-    //     },
-
-    //     get note() {
-    //         return note;
-    //     },
-
-    //     get dueDate() {
-    //         return dueDate;
-    //     },
-
-    //     get completed() {
-    //         return completed;
-    //     },
-        
-    //     get priority() {
-    //         return priority;
-    //     },
-
-    //     set taskName(name) {
-    //         return taskName = name;
-    //     },
-
-    //     set writeNote(desc) {
-    //         note = desc;
-    //     },
-
-    //     set date(date) {
-    //         return dueDate = date
-    //     },
-
-    //     set done(status) {
-    //         return completed = status
-    //     },
-
-    //     set priority(status) {
-    //         return priority = status;
-    //     }
-
-    // }
-
+    
 }
 
-export default newTask
+export default Task
