b25a686a6cb1db9211c6f103a3686b33cc02a72b
Jordan <jordansampson23@gmail.com>
2023-01-25 13:25:12 -0600

    handle event listeners for tiles

--- src/javascript/DOMController.js
+++ src/javascript/DOMController.js
@@ -3,11 +3,11 @@ import { projectList } from "./project"
 
 const openForm = () => {
     document.querySelector('.taskForm').style.display = 'block'
-}
+};
 
 const closeForm = () => {
     document.querySelector('.taskForm').style.display = 'none'
-}
+};
 
 const displayProjects = () => {
     const projectsDisplay = document.querySelector('.projects')
@@ -15,7 +15,7 @@ const displayProjects = () => {
         console.log(item.projectName)
         createTile(item.projectName)
     });
-}
+};
 
 const createTile = (projectName) => {
     const projectsDisplay = document.querySelector('.projects')
@@ -27,6 +27,21 @@ const createTile = (projectName) => {
     projectsDisplay.appendChild(tile)
 
     return tile;
+};
+
+const handleTileListeners = () => {
+    const projectTiles = document.querySelectorAll('.tile')
+    projectTiles.forEach(tile => {
+        tile.addEventListener('click', (event) => {
+            // displayTasks(event.target)
+        })
+    })
+}
+
+handleTileListeners()
+
+const displayTasks = (project) => {
+    // Display tasks from this.project list array
 }
 
 displayProjects()
