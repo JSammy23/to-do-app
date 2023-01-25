78ae42c36fe8add3e7683ccd31e71bea58abdbf8
Jordan <jordansampson23@gmail.com>
2023-01-25 11:56:48 -0600

    Removed module code test code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import './styles.css';
-import Task, {handleForm} from './javascript/task';
+import Task from './javascript/task';
 import { compareAsc, format } from 'date-fns'
 import Project, { projectList } from './javascript/project';
+import { openForm, closeForm } from './javascript/DOMController'; 
 
 
 
-//Handle default project
-const firstProject = Project('Default')
-projectList.push(firstProject)
-firstProject.newTask({
-    taskName: 'Mow the yard',
-    dueDate: format(new Date(2023, 0, 27), 'MM/dd/yyyy'),
-    note: 'Rain expected Thursday',
-    priority: 'normal'
-})
 
-console.log(firstProject)
 
