25bc4b601686b5fecccf6bdafc10e6fe7ee403f0
Jordan <jordansampson23@gmail.com>
2023-01-26 13:56:53 -0600

    Webpack commit

--- dist/main.js
+++ dist/main.js
@@ -26,7 +26,7 @@ var ___CSS_LOADER_URL_IMPORT_0___ = new URL(/* asset import */ __webpack_require
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
 var ___CSS_LOADER_URL_REPLACEMENT_0___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(___CSS_LOADER_URL_IMPORT_0___);
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "*, *::before, *::after {\n    margin: 0;\n    padding: 0;\n    box-sizing: border-box;\n}\n\n:root {\n    --dark-lightGray: #282828;\n    --dark-darkGray: #1f1f1f;\n    --dark-wfont: #fff;\n    --accent-pink: #df9dc9;\n}\n\nbody {\n    width: 100%;\n    height: 100%;\n    overflow: hidden;\n    background: url(" + ___CSS_LOADER_URL_REPLACEMENT_0___ + ");\n}\n\n#content {\n    width: 85vw;\n    height: 85vh;\n    margin-left: auto;\n    margin-right: auto;\n    margin-top: 50px;\n    /* border: 3px solid red; */\n    display: grid;\n    color: var(--dark-wfont);\n    grid-template-columns: 375px auto;\n    grid-template-rows: min-content;\n    box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.64);\n    -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.64);\n    -moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.64);;\n}\n\n#headerBar {\n    grid-column: 1 / span 2;\n    height: 40px;\n    border-bottom: 2px solid blueviolet;\n}\n\n#headerBar,\n#sideBar {\n    background-color: var(--dark-lightGray);\n    color: var(--dark-wfont);\n}\n\n#mainBody {\n    background-color: var(--dark-darkGray);\n\n}\n\n.tile {\n    background-color: var(--dark-darkGray);\n    padding: 8px;\n    cursor: pointer;\n    border-bottom: 1px solid blueviolet;\n}\n\n.card {\n    /* border: 2px solid red; */\n    border-radius: 10px;\n    background-color: var(--dark-lightGray);\n    display: flex;\n    /* justify-content: space-between; */\n    padding: 8px;\n    width: 85%;\n    margin-left: auto;\n    margin-right: auto;\n    margin-top: 20px;\n}\n\n.card > button {\n    background: none;\n    outline: none;\n    border: none;\n    cursor: pointer;\n}\n\n.taskCheckMark {\n    margin-right: 10px;\n}\n\n.taskEdit {\n    margin-left: auto;\n}\n\n.taskFolders {\n    border-bottom: 2px solid #fff;\n    min-height: 25%;\n    /* padding: 8px; */\n    margin-bottom: 1rem;\n}\n\n.taskForm {\n    /* Change display before production */\n    display: none; \n    position: fixed;\n    top: 25%;\n    left: 35%;\n    border: 2px solid var(--accent-pink);\n    width: 800px;\n    background-color: var(--dark-lightGray);\n    color: #fff;\n    padding: 15px;\n    border-radius: 15px;\n    font-size: 1.25rem;\n}\n\n.formFooter {\n    display: flex;\n    justify-content: end;\n}\n\n.formFooter > button {\n    background: none;\n    outline: none;\n    border: 2px solid var(--accent-pink);\n    border-radius: 15px;\n    color: var(--accent-pink);\n    padding: 8px;\n    cursor: pointer;\n}\n\ninput {\n    font-size: 1.25rem;\n}\n\ninput[type=text] {\n    width: 500px;\n}\n\nselect {\n    font-size: 1.25rem;\n    width: 200px;\n}\n\n.formLegend {\n    padding: 20px;\n}\n\n.formHeader {\n    display: flex;\n    justify-content: space-between;\n}\n\n#closeTask {\n    border: none;\n    outline: none;\n    background: none;\n    cursor: pointer;\n}", "",{"version":3,"sources":["webpack://./src/styles.css"],"names":[],"mappings":"AAAA;IACI,SAAS;IACT,UAAU;IACV,sBAAsB;AAC1B;;AAEA;IACI,yBAAyB;IACzB,wBAAwB;IACxB,kBAAkB;IAClB,sBAAsB;AAC1B;;AAEA;IACI,WAAW;IACX,YAAY;IACZ,gBAAgB;IAChB,mDAAwC;AAC5C;;AAEA;IACI,WAAW;IACX,YAAY;IACZ,iBAAiB;IACjB,kBAAkB;IAClB,gBAAgB;IAChB,2BAA2B;IAC3B,aAAa;IACb,wBAAwB;IACxB,iCAAiC;IACjC,+BAA+B;IAC/B,8CAA8C;IAC9C,sDAAsD;IACtD,mDAAmD;AACvD;;AAEA;IACI,uBAAuB;IACvB,YAAY;IACZ,mCAAmC;AACvC;;AAEA;;IAEI,uCAAuC;IACvC,wBAAwB;AAC5B;;AAEA;IACI,sCAAsC;;AAE1C;;AAEA;IACI,sCAAsC;IACtC,YAAY;IACZ,eAAe;IACf,mCAAmC;AACvC;;AAEA;IACI,2BAA2B;IAC3B,mBAAmB;IACnB,uCAAuC;IACvC,aAAa;IACb,oCAAoC;IACpC,YAAY;IACZ,UAAU;IACV,iBAAiB;IACjB,kBAAkB;IAClB,gBAAgB;AACpB;;AAEA;IACI,gBAAgB;IAChB,aAAa;IACb,YAAY;IACZ,eAAe;AACnB;;AAEA;IACI,kBAAkB;AACtB;;AAEA;IACI,iBAAiB;AACrB;;AAEA;IACI,6BAA6B;IAC7B,eAAe;IACf,kBAAkB;IAClB,mBAAmB;AACvB;;AAEA;IACI,qCAAqC;IACrC,aAAa;IACb,eAAe;IACf,QAAQ;IACR,SAAS;IACT,oCAAoC;IACpC,YAAY;IACZ,uCAAuC;IACvC,WAAW;IACX,aAAa;IACb,mBAAmB;IACnB,kBAAkB;AACtB;;AAEA;IACI,aAAa;IACb,oBAAoB;AACxB;;AAEA;IACI,gBAAgB;IAChB,aAAa;IACb,oCAAoC;IACpC,mBAAmB;IACnB,yBAAyB;IACzB,YAAY;IACZ,eAAe;AACnB;;AAEA;IACI,kBAAkB;AACtB;;AAEA;IACI,YAAY;AAChB;;AAEA;IACI,kBAAkB;IAClB,YAAY;AAChB;;AAEA;IACI,aAAa;AACjB;;AAEA;IACI,aAAa;IACb,8BAA8B;AAClC;;AAEA;IACI,YAAY;IACZ,aAAa;IACb,gBAAgB;IAChB,eAAe;AACnB","sourcesContent":["*, *::before, *::after {\n    margin: 0;\n    padding: 0;\n    box-sizing: border-box;\n}\n\n:root {\n    --dark-lightGray: #282828;\n    --dark-darkGray: #1f1f1f;\n    --dark-wfont: #fff;\n    --accent-pink: #df9dc9;\n}\n\nbody {\n    width: 100%;\n    height: 100%;\n    overflow: hidden;\n    background: url(./assets/img/forest.jpg);\n}\n\n#content {\n    width: 85vw;\n    height: 85vh;\n    margin-left: auto;\n    margin-right: auto;\n    margin-top: 50px;\n    /* border: 3px solid red; */\n    display: grid;\n    color: var(--dark-wfont);\n    grid-template-columns: 375px auto;\n    grid-template-rows: min-content;\n    box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.64);\n    -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.64);\n    -moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.64);;\n}\n\n#headerBar {\n    grid-column: 1 / span 2;\n    height: 40px;\n    border-bottom: 2px solid blueviolet;\n}\n\n#headerBar,\n#sideBar {\n    background-color: var(--dark-lightGray);\n    color: var(--dark-wfont);\n}\n\n#mainBody {\n    background-color: var(--dark-darkGray);\n\n}\n\n.tile {\n    background-color: var(--dark-darkGray);\n    padding: 8px;\n    cursor: pointer;\n    border-bottom: 1px solid blueviolet;\n}\n\n.card {\n    /* border: 2px solid red; */\n    border-radius: 10px;\n    background-color: var(--dark-lightGray);\n    display: flex;\n    /* justify-content: space-between; */\n    padding: 8px;\n    width: 85%;\n    margin-left: auto;\n    margin-right: auto;\n    margin-top: 20px;\n}\n\n.card > button {\n    background: none;\n    outline: none;\n    border: none;\n    cursor: pointer;\n}\n\n.taskCheckMark {\n    margin-right: 10px;\n}\n\n.taskEdit {\n    margin-left: auto;\n}\n\n.taskFolders {\n    border-bottom: 2px solid #fff;\n    min-height: 25%;\n    /* padding: 8px; */\n    margin-bottom: 1rem;\n}\n\n.taskForm {\n    /* Change display before production */\n    display: none; \n    position: fixed;\n    top: 25%;\n    left: 35%;\n    border: 2px solid var(--accent-pink);\n    width: 800px;\n    background-color: var(--dark-lightGray);\n    color: #fff;\n    padding: 15px;\n    border-radius: 15px;\n    font-size: 1.25rem;\n}\n\n.formFooter {\n    display: flex;\n    justify-content: end;\n}\n\n.formFooter > button {\n    background: none;\n    outline: none;\n    border: 2px solid var(--accent-pink);\n    border-radius: 15px;\n    color: var(--accent-pink);\n    padding: 8px;\n    cursor: pointer;\n}\n\ninput {\n    font-size: 1.25rem;\n}\n\ninput[type=text] {\n    width: 500px;\n}\n\nselect {\n    font-size: 1.25rem;\n    width: 200px;\n}\n\n.formLegend {\n    padding: 20px;\n}\n\n.formHeader {\n    display: flex;\n    justify-content: space-between;\n}\n\n#closeTask {\n    border: none;\n    outline: none;\n    background: none;\n    cursor: pointer;\n}"],"sourceRoot":""}]);
+___CSS_LOADER_EXPORT___.push([module.id, "*, *::before, *::after {\n    margin: 0;\n    padding: 0;\n    box-sizing: border-box;\n}\n\n:root {\n    --dark-lightGray: #282828;\n    --dark-darkGray: #1f1f1f;\n    --dark-wfont: #fff;\n    --accent-pink: #df9dc9;\n}\n\nbody {\n    width: 100%;\n    height: 100%;\n    overflow: hidden;\n    background: url(" + ___CSS_LOADER_URL_REPLACEMENT_0___ + ");\n}\n\n#content {\n    width: 85vw;\n    height: 85vh;\n    margin-left: auto;\n    margin-right: auto;\n    margin-top: 50px;\n    /* border: 3px solid red; */\n    display: grid;\n    color: var(--dark-wfont);\n    grid-template-columns: 375px auto;\n    grid-template-rows: min-content;\n    box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.64);\n    -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.64);\n    -moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.64);;\n}\n\n#headerBar {\n    grid-column: 1 / span 2;\n    height: 40px;\n    border-bottom: 2px solid blueviolet;\n}\n\n#headerBar,\n#sideBar {\n    background-color: var(--dark-lightGray);\n    color: var(--dark-wfont);\n}\n\n#mainBody {\n    background-color: var(--dark-darkGray);\n\n}\n\n.tile {\n    background-color: var(--dark-darkGray);\n    padding: 8px;\n    cursor: pointer;\n    border-bottom: 1px solid blueviolet;\n}\n\n.card {\n    /* border: 2px solid red; */\n    border-radius: 10px;\n    background-color: var(--dark-lightGray);\n    display: flex;\n    /* justify-content: space-between; */\n    padding: 8px;\n    width: 85%;\n    margin-left: auto;\n    margin-right: auto;\n    margin-top: 20px;\n}\n\n.note {\n    color: rgba(255, 255, 255, .5);\n}\n\n.card > button {\n    background: none;\n    outline: none;\n    border: none;\n    cursor: pointer;\n}\n\n.taskCheckMark {\n    margin-right: 10px;\n}\n\n.taskEdit {\n    margin-left: auto;\n}\n\n.taskFolders {\n    border-bottom: 2px solid #fff;\n    min-height: 25%;\n    /* padding: 8px; */\n    margin-bottom: 1rem;\n}\n\n.taskForm {\n    /* Change display before production */\n    display: none; \n    position: fixed;\n    top: 25%;\n    left: 35%;\n    border: 2px solid var(--accent-pink);\n    width: 800px;\n    background-color: var(--dark-lightGray);\n    color: #fff;\n    padding: 15px;\n    border-radius: 15px;\n    font-size: 1.25rem;\n}\n\n.formFooter {\n    display: flex;\n    justify-content: end;\n}\n\n.formFooter > button {\n    background: none;\n    outline: none;\n    border: 2px solid var(--accent-pink);\n    border-radius: 15px;\n    color: var(--accent-pink);\n    padding: 8px;\n    cursor: pointer;\n}\n\ninput {\n    font-size: 1.25rem;\n}\n\ninput[type=text] {\n    width: 500px;\n}\n\nselect {\n    font-size: 1.25rem;\n    width: 200px;\n}\n\n.formLegend {\n    padding: 20px;\n}\n\n.formHeader {\n    display: flex;\n    justify-content: space-between;\n}\n\n#closeTask {\n    border: none;\n    outline: none;\n    background: none;\n    cursor: pointer;\n}", "",{"version":3,"sources":["webpack://./src/styles.css"],"names":[],"mappings":"AAAA;IACI,SAAS;IACT,UAAU;IACV,sBAAsB;AAC1B;;AAEA;IACI,yBAAyB;IACzB,wBAAwB;IACxB,kBAAkB;IAClB,sBAAsB;AAC1B;;AAEA;IACI,WAAW;IACX,YAAY;IACZ,gBAAgB;IAChB,mDAAwC;AAC5C;;AAEA;IACI,WAAW;IACX,YAAY;IACZ,iBAAiB;IACjB,kBAAkB;IAClB,gBAAgB;IAChB,2BAA2B;IAC3B,aAAa;IACb,wBAAwB;IACxB,iCAAiC;IACjC,+BAA+B;IAC/B,8CAA8C;IAC9C,sDAAsD;IACtD,mDAAmD;AACvD;;AAEA;IACI,uBAAuB;IACvB,YAAY;IACZ,mCAAmC;AACvC;;AAEA;;IAEI,uCAAuC;IACvC,wBAAwB;AAC5B;;AAEA;IACI,sCAAsC;;AAE1C;;AAEA;IACI,sCAAsC;IACtC,YAAY;IACZ,eAAe;IACf,mCAAmC;AACvC;;AAEA;IACI,2BAA2B;IAC3B,mBAAmB;IACnB,uCAAuC;IACvC,aAAa;IACb,oCAAoC;IACpC,YAAY;IACZ,UAAU;IACV,iBAAiB;IACjB,kBAAkB;IAClB,gBAAgB;AACpB;;AAEA;IACI,8BAA8B;AAClC;;AAEA;IACI,gBAAgB;IAChB,aAAa;IACb,YAAY;IACZ,eAAe;AACnB;;AAEA;IACI,kBAAkB;AACtB;;AAEA;IACI,iBAAiB;AACrB;;AAEA;IACI,6BAA6B;IAC7B,eAAe;IACf,kBAAkB;IAClB,mBAAmB;AACvB;;AAEA;IACI,qCAAqC;IACrC,aAAa;IACb,eAAe;IACf,QAAQ;IACR,SAAS;IACT,oCAAoC;IACpC,YAAY;IACZ,uCAAuC;IACvC,WAAW;IACX,aAAa;IACb,mBAAmB;IACnB,kBAAkB;AACtB;;AAEA;IACI,aAAa;IACb,oBAAoB;AACxB;;AAEA;IACI,gBAAgB;IAChB,aAAa;IACb,oCAAoC;IACpC,mBAAmB;IACnB,yBAAyB;IACzB,YAAY;IACZ,eAAe;AACnB;;AAEA;IACI,kBAAkB;AACtB;;AAEA;IACI,YAAY;AAChB;;AAEA;IACI,kBAAkB;IAClB,YAAY;AAChB;;AAEA;IACI,aAAa;AACjB;;AAEA;IACI,aAAa;IACb,8BAA8B;AAClC;;AAEA;IACI,YAAY;IACZ,aAAa;IACb,gBAAgB;IAChB,eAAe;AACnB","sourcesContent":["*, *::before, *::after {\n    margin: 0;\n    padding: 0;\n    box-sizing: border-box;\n}\n\n:root {\n    --dark-lightGray: #282828;\n    --dark-darkGray: #1f1f1f;\n    --dark-wfont: #fff;\n    --accent-pink: #df9dc9;\n}\n\nbody {\n    width: 100%;\n    height: 100%;\n    overflow: hidden;\n    background: url(./assets/img/forest.jpg);\n}\n\n#content {\n    width: 85vw;\n    height: 85vh;\n    margin-left: auto;\n    margin-right: auto;\n    margin-top: 50px;\n    /* border: 3px solid red; */\n    display: grid;\n    color: var(--dark-wfont);\n    grid-template-columns: 375px auto;\n    grid-template-rows: min-content;\n    box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.64);\n    -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.64);\n    -moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.64);;\n}\n\n#headerBar {\n    grid-column: 1 / span 2;\n    height: 40px;\n    border-bottom: 2px solid blueviolet;\n}\n\n#headerBar,\n#sideBar {\n    background-color: var(--dark-lightGray);\n    color: var(--dark-wfont);\n}\n\n#mainBody {\n    background-color: var(--dark-darkGray);\n\n}\n\n.tile {\n    background-color: var(--dark-darkGray);\n    padding: 8px;\n    cursor: pointer;\n    border-bottom: 1px solid blueviolet;\n}\n\n.card {\n    /* border: 2px solid red; */\n    border-radius: 10px;\n    background-color: var(--dark-lightGray);\n    display: flex;\n    /* justify-content: space-between; */\n    padding: 8px;\n    width: 85%;\n    margin-left: auto;\n    margin-right: auto;\n    margin-top: 20px;\n}\n\n.note {\n    color: rgba(255, 255, 255, .5);\n}\n\n.card > button {\n    background: none;\n    outline: none;\n    border: none;\n    cursor: pointer;\n}\n\n.taskCheckMark {\n    margin-right: 10px;\n}\n\n.taskEdit {\n    margin-left: auto;\n}\n\n.taskFolders {\n    border-bottom: 2px solid #fff;\n    min-height: 25%;\n    /* padding: 8px; */\n    margin-bottom: 1rem;\n}\n\n.taskForm {\n    /* Change display before production */\n    display: none; \n    position: fixed;\n    top: 25%;\n    left: 35%;\n    border: 2px solid var(--accent-pink);\n    width: 800px;\n    background-color: var(--dark-lightGray);\n    color: #fff;\n    padding: 15px;\n    border-radius: 15px;\n    font-size: 1.25rem;\n}\n\n.formFooter {\n    display: flex;\n    justify-content: end;\n}\n\n.formFooter > button {\n    background: none;\n    outline: none;\n    border: 2px solid var(--accent-pink);\n    border-radius: 15px;\n    color: var(--accent-pink);\n    padding: 8px;\n    cursor: pointer;\n}\n\ninput {\n    font-size: 1.25rem;\n}\n\ninput[type=text] {\n    width: 500px;\n}\n\nselect {\n    font-size: 1.25rem;\n    width: 200px;\n}\n\n.formLegend {\n    padding: 20px;\n}\n\n.formHeader {\n    display: flex;\n    justify-content: space-between;\n}\n\n#closeTask {\n    border: none;\n    outline: none;\n    background: none;\n    cursor: pointer;\n}"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -3504,12 +3504,12 @@ const closeForm = () => {
 const displayProjects = () => {
     const projectsDisplay = document.querySelector('.projects')
     _project__WEBPACK_IMPORTED_MODULE_0__.projectList.forEach(item => {
-        console.log(item.projectName)
         createTile(item.projectName)
     });
 };
 
 const createTile = (projectName) => {
+    const body = document.getElementById('mainBody')
     const projectsDisplay = document.querySelector('.projects')
     const tile = document.createElement('div')
     tile.classList.add('tile')
@@ -3519,7 +3519,7 @@ const createTile = (projectName) => {
     projectsDisplay.appendChild(tile)
     // This puts listeners on dynamic elements
     tile.addEventListener('click', (event) => {
-        console.log(event.target.textContent)
+        body.textContent = '' // Refresh body every click to avoid duplicates
         displayTasks(event.target.textContent)
     })
 
@@ -3527,14 +3527,20 @@ const createTile = (projectName) => {
 };
 
 // This is only working on static elements
-const handleTileListeners = () => {
-    const projectTiles = document.querySelectorAll('.tile')
-    projectTiles.forEach(tile => {
-        tile.addEventListener('click', (event) => {
-            // displayTasks(event.target)
-            
-        })
+const handleAllTaskListener = () => {
+    const body = document.getElementById('mainBody')
+    const allTasksTile = document.getElementById('allTasks')
+    allTasksTile.addEventListener('click', (event) => {
+        body.textContent = ''
+        displayAllTasks()
     })
+}
+handleAllTaskListener()
+
+const displayAllTasks = () => {
+    for (let i = 0; i < _project__WEBPACK_IMPORTED_MODULE_0__.allTasks.length; i++) {
+        createCard(_project__WEBPACK_IMPORTED_MODULE_0__.allTasks[i])
+    }
 }
 
 
@@ -3556,11 +3562,13 @@ const createCard = (task) => {
     const body = document.getElementById('mainBody')
     const card = document.createElement('div')
     card.classList.add('card')
-    //Handle title
+    // Handle title // Title & note need to be in same div
+    const titleDiv = document.createElement('div')
     const title = document.createElement('h3')
     title.textContent = task.taskName
+    titleDiv.appendChild(title)
 
-    //Handle complete button
+    // Handle complete button
     const checkBtn = document.createElement('button')
     checkBtn.classList.add('taskCheckMark')
     const circleSvg = document.createElementNS("http://www.w3.org/2000/svg", 'svg')
@@ -3574,6 +3582,12 @@ const createCard = (task) => {
 
     circleSvg.appendChild(circlePath)
     checkBtn.appendChild(circleSvg)
+
+    // Handle note
+    const note = document.createElement('p')
+    note.classList.add('note')
+    note.textContent = task.note
+    titleDiv.appendChild(note)
 
     // Handle edit button
     const editBtn = document.createElement('button')
@@ -3592,7 +3606,7 @@ const createCard = (task) => {
 
     // Append card in order 
     card.appendChild(checkBtn)
-    card.appendChild(title)
+    card.appendChild(titleDiv)
     card.appendChild(editBtn)
     body.appendChild(card)
 }
@@ -3612,6 +3626,7 @@ displayProjects()
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "allTasks": () => (/* binding */ allTasks),
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__),
 /* harmony export */   "projectList": () => (/* binding */ projectList)
 /* harmony export */ });
@@ -3654,6 +3669,13 @@ defaultProject.newTask({
     priority: 'normal'
 })
 
+defaultProject.newTask({
+    taskName: 'Testing a really long title to see if I need to use flex wrap on my title for taskName',
+    dueDate: (0,date_fns__WEBPACK_IMPORTED_MODULE_1__["default"])(new Date(2023, 0, 30), 'MM/dd/yyyy'),
+    note: 'Where will I display the note?',
+    priority: 'normal'
+})
+
 const testProj = Project('Test')
 projectList.push(testProj)
 testProj.newTask({
@@ -3663,7 +3685,20 @@ testProj.newTask({
     priority: 'low'
 })
 
-console.log(projectList)
+// Gather all tasks into single array
+const grabTasks = []
+const allTasks = []
+projectList.forEach(item => {
+    grabTasks.push(item.tasks)
+})
+console.log(grabTasks)
+for (let i = 0; i < grabTasks.length; i++) {
+    for (let j = 0; j < grabTasks[i].length; j++) {
+        allTasks.push(grabTasks[i][j])
+    }
+}
+console.log(allTasks)
+
 
 
 
