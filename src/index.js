1c2bdc3c93086ec43f9c35042d78cb36df6079a6
Jordan <jordansampson23@gmail.com>
2023-01-28 11:44:27 -0600

    Swapping imports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles.css';
 import Task from './javascript/task';
 import { compareAsc, format } from 'date-fns'
 import Project, { projectList, allTasks } from './javascript/project';
-import { openForm, closeForm } from './javascript/DOMController'; 
+import { activeProject } from './javascript/DOMController'; 
 
 
 
