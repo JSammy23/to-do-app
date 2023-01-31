873482c219a032e28300b3301e927b63b84da2dc
Jordan <jordansampson23@gmail.com>
2023-01-31 02:42:16 -0600

    Cleaned up imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import './styles.css';
-import Task from './javascript/task';
-import { compareAsc, format } from 'date-fns'
-import Project, { projectList, allTasks } from './javascript/project';
+import { projectList, allTasks } from './javascript/project';
 import { activeProject } from './javascript/DOMController'; 
 
 
